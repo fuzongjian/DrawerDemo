2d33080b01c6f8f43c070148f18022e3346325a7
fuzongjian <2917066078@qq.com>
2016-03-08 10:41:31 +0800

    1.0
    初始化项目

---
+++ DrawerDemo/DrawerDemo/ViewController/SuperViewController/SuperViewController.h
@@ -0,0 +1,15 @@
+//
+//  SuperViewController.h
+//  DrawerDemo
+//
+//  Created by fdkj0002 on 16/3/8.
+//  Copyright © 2016年 FZJ.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuperViewController : UIViewController
+//自定义标题,以及标题颜色
+-(void)setCustomTitle:(NSString *)title titleFont:(CGFloat)fontSize;
+
+@end
